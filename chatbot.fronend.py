0b2aa62f1dbc739a34eda8965167fcb60227f67b
ronak-navadiya <ronaknavadiya123@gmail.com>
2025-08-27 12:44:03 -0400

    comments

--- chatbot.fronend.py
+++ chatbot.fronend.py
@@ -5,6 +5,8 @@ from langchain_core.messages import HumanMessage
 
 CONFIG = {'configurable': {'thread_id': 'thread-1'}}
 
+#------------------------------------   Init Setup ------------------------------------#
+
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
 
@@ -14,7 +16,7 @@ for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
         st.text(message['content'])
 
-
+# ------------------------------------------Screen  UI ---------------------------------------- #
 user_input = st.chat_input("Type here...")
 
 
